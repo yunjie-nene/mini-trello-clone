0934e780d64c72ee739757c26e482f23d173abd5
Nora <nora.yunjie@gmail.com>
2025-04-30 13:54:29 +1200

    refactor: remove board, card, and list controllers and their routes

--- server/src/routes/listRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getLists, createList } from '../controllers/listController';
-
-const router = express.Router();
-
-router.get('/', getLists);
-router.post('/', createList);
-
-export default router;
