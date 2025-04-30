2f4c2b41601257e2209b500701693256e493fac0
Nora <nora.yunjie@gmail.com>
2025-04-30 22:49:11 +1200

    refactor: update Apollo Client HTTP link to use environment variable for backend URL

--- client/src/apolloClient.ts
+++ client/src/apolloClient.ts
@@ -1,8 +1,10 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3000/graphql'
+  uri: `${import.meta.env.VITE_BE_BASE_URL}/graphql`
 });
+
+console.log('Apollo Client HTTP Link:', import.meta.env);
 
 const client = new ApolloClient({
   link: httpLink,
