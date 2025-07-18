a296655df83b3e909623cdbfe5f1a591a076cc6f
Nora <nora.yunjie@gmail.com>
2025-07-18 20:51:44 +1200

    allow config cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,10 @@ dotenv.config();
 async function startServer() {
   const app = express();
 
-  app.use(cors());
+  app.use(cors({
+    origin: process.env.CORS_ORIGIN || '*',
+    credentials: true,
+  }));
   app.use(express.json());
 
   connectDB();
